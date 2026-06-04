66dffc4d8317b130e797eb9ebb8cc4148fad9770
riqqahluth-pixel <riqqahluth@gmail.com>
2026-06-04 22:29:03 +0800

    Added courses, assessments, tasks, attempts tables

--- schema.sql
+++ schema.sql
@@ -8,3 +8,36 @@ CREATE TABLE students (
     password_hash VARCHAR(255) NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE assessments (
+    assessment_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT,
+    title VARCHAR(100),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+
+CREATE TABLE tasks (
+    task_id INT AUTO_INCREMENT PRIMARY KEY,
+    assessment_id INT,
+    title VARCHAR(100),
+    FOREIGN KEY (assessment_id) REFERENCES assessments(assessment_id)
+);
+
+
+CREATE TABLE attempts (
+    attempt_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_username VARCHAR(50),
+    task_id INT,
+    score INT,
+
+    FOREIGN KEY (student_username) REFERENCES students(username),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+);
