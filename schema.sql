617f6fe9607797aeb33cd95733bc702f91e59313
Ihsan2489 <m.ihsan.sudarman@gmail.com>
2026-06-06 10:38:27 +0800

    changes to students schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE students (
     -- 1. This handles the actual auto-incrementing math
     id INT AUTO_INCREMENT PRIMARY KEY,
     -- 2. This automatically prepends "student_" to the ID
-    student_id VARCHAR(20) GENERATED ALWAYS AS (CONCAT('student_', id)) STORED,
+    -- student_id VARCHAR(20) GENERATED ALWAYS AS (CONCAT('student_', id)) STORED,
     username VARCHAR(50) NOT NULL UNIQUE,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
